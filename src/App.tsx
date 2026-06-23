7b1b387faa8f9f7aa99b4801288b9c41c4e20465
phantom099 <olegbagryantsev@gmail.com>
2026-06-23 22:37:24 +0300

    filters & sorts added

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,28 @@ function App() {
   const [candidates, setCandidates] = useState<Candidate[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
+  const [showOnlyWithContact, setShowOnlyWithContact] = useState(false);
+  const [sourceFilter, setSourceFilter] = useState<'hh_api' | 'p_local' | 'all'>('all');
+  const [sortBy, setSortBy] = useState<'relevance_score' | 'found_skills'>('relevance_score');
+
+  let filteredCandidates = [...candidates];
+  if (sourceFilter !== 'all') {
+    filteredCandidates = filteredCandidates.filter(candidate => candidate.source === sourceFilter)
+  }; 
+  if (showOnlyWithContact) {
+    filteredCandidates = filteredCandidates.filter(candidate => candidate.contact_available)
+  }; 
+
+  if (sortBy === 'relevance_score') {
+    filteredCandidates.sort((a, b) => b.relevance_score - a.relevance_score)
+  }
+  if (sortBy === 'found_skills') {
+    filteredCandidates.sort((a, b) => {
+      const aFoundSkills = a.skills_coverage.filter(skill => skill.status === 'found').length;
+      const bFoundSkills = b.skills_coverage.filter(skill => skill.status === 'found').length;
+      return bFoundSkills - aFoundSkills;
+    })
+  }
 
   useEffect(() => {
     const fetchData = async () => {
@@ -31,9 +53,60 @@ function App() {
 
   return (
     <div>
+      <button onClick={() => setShowOnlyWithContact(prev => !prev)}>
+        Только с контактами 
+      </button>
+      <label>
+        <input 
+        type="radio" 
+        name='sourceFilter' 
+        value='all' 
+        checked={sourceFilter ==='all'} 
+        onChange={() => setSourceFilter('all')}/> 
+        Все
+      </label>
+      <label>
+        <input 
+        type="radio" 
+        name='sourceFilter' 
+        value='hh_api' 
+        checked={sourceFilter ==='hh_api'} 
+        onChange={() => setSourceFilter('hh_api')}/> 
+        HH.ru
+      </label>
+      <label>
+        <input 
+        type="radio" 
+        name='sourceFilter' 
+        value='p_local' 
+        checked={sourceFilter ==='p_local'} 
+        onChange={() => setSourceFilter('p_local')}/> 
+        Локальные
+      </label>
+      <label>
+  <input
+    type="radio"
+    name="sortBy"
+    value="relevance"
+    checked={sortBy === 'relevance_score'}
+    onChange={() => setSortBy('relevance_score')}
+  />
+  По релевантности
+</label>
+
+<label>
+  <input
+    type="radio"
+    name="sortBy"
+    value="found_skills"
+    checked={sortBy === 'found_skills'}
+    onChange={() => setSortBy('found_skills')}
+  />
+  По найденным навыкам
+</label>
       <h1>Кандидаты</h1>
 
-      {candidates.map(candidate => (
+      {filteredCandidates.map(candidate => (
         <CandidateCard key={candidate.id} candidate={candidate}/>
       ))}
     </div>
